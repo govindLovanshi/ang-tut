71f96ce7a735d5d11f0fb235137d01b67cdf32e6
Govind Lovanshi <govind.lowanshi@geexu.in>
2023-03-17 01:07:18 +0530

    in this I have just make diffrent types of components with cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // component declarations
+    AppComponent,
+    NavComponent,
+    HeaderComponent, // it automtic get added here
   ],
-  imports: [
+  imports: [ // model imports
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // dependency injection
+  bootstrap: [AppComponent] // render the component
 })
 export class AppModule { }
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({ // decorators
+  selector: 'app-nav', //tag Name
+  templateUrl: './nav.component.html', // HTML page
+  styleUrls: ['./nav.component.css'] // CSS styles
+})
+export class NavComponent {
+
+}

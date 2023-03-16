b4ce2b8942def1e1c2773ca0d35c2e87500a0ec9
Govind Lovanshi <govind.lowanshi@geexu.in>
2023-03-16 19:22:28 +0530

    NgStyle & NgClass - Built-in Attribute Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+   
+// ng Style
+  myStyles : any = {
+    width : "100px",
+    height : "150px",
+    backgroundColor : "red",
+    color :'yellow'
+  }
+
+
+  addStyle(){
+    this.myStyles['color'] = 'black',
+    this.myStyles['border'] = '2px solid black'
+  }
+
+  // ng class
+
+  myClass :any = {
+    box : true,
+    border : false,
+    circles : false,
+  }
+
+  chnageShape(){ // true --> false (dynamic)
+    this.myClass.border = !this.myClass.border,
+    this.myClass.circle = !this.myClass.circle
+  }
+
+
 
 
 }

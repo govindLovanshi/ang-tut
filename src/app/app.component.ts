7ce89bbfad12bdc5144d95754e1716aff248e6f1
Govind Lovanshi <govind.lowanshi@geexu.in>
2023-03-16 16:09:59 +0530

    attribute binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang-tut';
-  url =  "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGljfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-  sal = 12000
-  show(){
-    return this.title + '-' + this.sal
-  }
+ 
+  btn : string = 'paymnet getway'
 
 }

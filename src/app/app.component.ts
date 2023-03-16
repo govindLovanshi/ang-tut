b42bff28ee7fd9ad479d266a382b6e83db141357
Govind Lovanshi <govind.lowanshi@geexu.in>
2023-03-16 21:50:22 +0530

    NgFor - Built-in Structural Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  currentUser : any = null;
+  isDisablebe = true;
+
+
+  names : string[] = ['govind' , 'abhishek' , 'hari' , 'sanket' , 'sushant'];
+
+
+  newNames = [
+    {
+      name: 'Abhishek',
+      age: 20,
+      gender:'male'
+    },
+    {
+      name: 'Govind',
+      age: 30,
+      gender: 'male'
+    },
+    {
+      name: 'Hari',
+      age: 40,
+      gender:'male'
+    },
+    {
+      name: 'Sanket',
+      age: 50,
+      gender:'male'
+    }
+  ]
+
+  showAndEdit(index : number ){
+    this.isDisablebe = true;
+    this.currentUser = this.newNames[index]
+  }
+
+  setValue(event : any){
+    const keyName = event.target.name;
+    const val = event.target.value;
+    this.currentUser[keyName] = val;
+
+
+  }
+  enableEditing(){
+    this.isDisablebe = false;
+  }
+  
 
 }

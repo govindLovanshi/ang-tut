7b2853756f21e60840740057a5913f7a9323e781
Govind Lovanshi <govind.lowanshi@geexu.in>
2023-03-16 17:04:16 +0530

    Event Binding - Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang-tut';
-  url =  "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGljfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-  sal = 12000
-  show(){
-    return this.title + '-' + this.sal
+
+
+  name : string = '';
+  uname : string = '';
+
+  handleClick(){
+    console.log('clicked by click me ');
   }
+
+  handleInput(event : any){
+    // if(event.key != 'Enter'){
+    //   return;
+    // }
+    let inputhere = event.target.value;
+    console.log(inputhere)
+    this.uname = inputhere;
+  }
+
+handleSlect(event : any){
+  let val = event.target.value;
+  console.log(val)
+
+}
+
 
 }

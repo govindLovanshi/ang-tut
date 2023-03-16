421b3af571c6a141e9932b8b9c4bf81261025a27
Govind Lovanshi <govind.lowanshi@geexu.in>
2023-03-17 00:13:00 +0530

    pipes and their uses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  name : string = 'gOViND LOvanshI';
+  today = new Date;
+  num = 2.567;
+  percentage = 1.234;
+  myCurrency = 7.23;
 
+  persons = {
+    name : 'John',
+    age : 20,
+    country : 'USA'
+  }
+
+  mySclice = 'mynameisgovind';
+  
 }

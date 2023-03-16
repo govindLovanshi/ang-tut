40975ebc71e388ec53744e7dfaaea356adb9a5e0
Govind Lovanshi <govind.lowanshi@geexu.in>
2023-03-16 19:50:09 +0530

    NgIf - Built-in Structural Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+   
 
+  constructor(){
+    this.getData()
+  }
+
+  // ng-if
+
+  selectedSkills : any = '' ;
+
+  handleEvent(event : any){
+    const value = event.target.value;
+    this.selectedSkills = value;
+
+  }
+
+  // 2nd exapmle 
+
+  data : any = '' ;
+  isDataArrived :boolean = false;
+
+  getData(){
+    setTimeout(()=>{
+      this.data = 'data from server',
+      this.isDataArrived = true;
+
+    } ,4000)
+  }
 
 }

394a7f081aa9b6ac71ee833533cb12d9c0df5b20
Govind Lovanshi <govind.lowanshi@geexu.in>
2023-03-21 12:14:07 +0530

    custom-pipe

---
+++ src/app/pipe/usd-inr.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'usdInr'
+})
+export class UsdInrPipe implements PipeTransform {
+
+  transform(value: number, ...args: number[]): number {
+    const [x] = args;
+
+    return value * x ;
+  }
+
+}

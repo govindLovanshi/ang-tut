ae3890fac58b7e8a08809a10c64ffac7aa0c9cff
Govind Lovanshi <govind.lowanshi@geexu.in>
2023-03-21 11:48:03 +0530

    ngAfterViewInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component , AfterViewInit, ViewChild, ElementRef, Directive } from '@angular/core';
+
+@Directive({
+  selector : "[fileInput]"
+})
+
+export class FileDirective {
+  constructor(public eleRef : ElementRef ) {}
+}
 
 @Component({ // component decoratiors 
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  
+  @ViewChild(FileDirective) inpRef! : FileDirective;
+
+  constructor(){
+
+  }
+  
+  ngAfterViewInit(): void {
+    const inp = this.inpRef.eleRef.nativeElement;
+
+    inp.addEventListener("change "  , ()=> {
+      console.log(inp.files);
+    })
+
+   console.log(FileDirective) 
+  }
 
 
 }

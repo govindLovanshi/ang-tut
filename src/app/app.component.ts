661424445f6e6a9aaee5d161ffc692620397ae5a
Govind Lovanshi <govind.lowanshi@geexu.in>
2023-03-21 13:00:02 +0530

    advance-pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+ 
+  title = "AppComponent";
+  today = Date();
+  user = {
+    name : 'govind',
+    age :24
+  }
 
 }
